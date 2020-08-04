baaf9f35299ff648da8bc627c757ffa922dd5b53
Andrei <andsemenov@gmail.com>
2020-08-04 01:05:45 +0100

    Working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import TablesGenerator from "./Tables_generator.js";
 import "./App.css";
 import allCountryScores from "./score.js";
 
 function App() {
-  return <TablesGenerator infoScores={allCountryScores} />;
+  let [state, setState] = useState("Ascending");
+  let [sort, setSort] = useState();
+  function sortWhenClicked() {
+    if (state === "Ascending") {
+      setState((state = "Descending"));
+    } else {
+      setState((state = "Ascending"));
+    }
+    console.log(state);
+    console.log(sort);
+  }
+  return (
+    <div>
+      <ButtonOnTheTop state={state} act={sortWhenClicked} />
+      <TablesGenerator
+        infoScores={sortCountriesAlphabetically(allCountryScores)}
+        playerInfo={allCountryScores}
+      />
+    </div>
+  );
+}
+
+function sortCountriesAlphabetically(array) {
+  let countryScores = array.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  return countryScores;
+}
+
+function ButtonOnTheTop(props) {
+  return <button onClick={props.act}>{props.state}</button>;
 }
 
 export default App;
--- src/Tables_generator.js
+++ src/Tables_generator.js
@@ -13,21 +13,38 @@ function TablesGenerator(props) {
   );
 }
 
+const PlayerScore = (props) =>
+  sortPlayersScoresAscending(props.playerInfo).map((name, index) => (
+    <td key={-index}>
+      {name.n} {name.s}
+    </td>
+  ));
+
 function HighScoreTable(props) {
-  return props.info.map((infoScore) => (
+  return props.info.map((infoScore, index) => (
     <tfoot>
       <tr>
-        <th scope="col">HIGH SCORES: {infoScore.name}</th>
+        <th key={index} scope="col">
+          HIGH SCORES: {infoScore.name}
+        </th>
       </tr>
       <tr className="row">
-        {infoScore.scores.map((name) => (
-          <td>
-            {name.n} {name.s}
-          </td>
-        ))}
+        <PlayerScore playerInfo={infoScore.scores} />
       </tr>
     </tfoot>
   ));
 }
+function sortPlayersScoresDescending(array) {
+  array.sort(function (a, b) {
+    return b.s - a.s;
+  });
+  return array;
+}
 
+function sortPlayersScoresAscending(array) {
+  array.sort(function (a, b) {
+    return a.s - b.s;
+  });
+  return array;
+}
 export default TablesGenerator;
